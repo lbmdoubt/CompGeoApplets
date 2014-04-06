5d09925baa9964e813acd9749a5c3159a57132cb
lbmdoubt <lbmdoubt@gmail.com>
2014-04-06 16:26:32 -0500

    dded point drawing and mouse positioning to voronoi

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,20 +2,3 @@ $(function() {
     // setup ul.tabs to work as tabs for each div directly under div.panes
     $("ul.tabs").tabs("div.panes > div");
 });
-
-function clearCanvas()
-{
-    var c=$(".panes div:not(:hidden) canvas")[0];
-    var ctx=c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
-}
-
-function drawPoint(canvasId)
-{
-    var c=document.getElementById(canvasId);
-    var ctx=c.getContext("2d");
-    ctx.save();
-    ctx.fillStyle="#FF0000";
-    ctx.fillRect(0,0,150,75);
-    ctx.restore();
-}
--- scripts/voronoi.js
+++ scripts/voronoi.js
@@ -1,4 +1,50 @@
+var pos;
+var points = new Array();
+var canvas;
+
 $(document).ready(function(){
-    var canvas = document.getElementById('voronoiCanvas');
-    canvas.addEventListener('click', function(){ drawPoint('voronoiCanvas')}, false);
+    //set up action listeners
+    canvas = document.getElementById('voronoiCanvas');
+    canvas.addEventListener('click', function(){ addPoint('voronoiCanvas')}, false);
+    canvas.addEventListener('mousemove', function(e) {
+        pos = getCanvasPos(e); /// provide this canvas and event
+    }, false);
+    //fix the canvas scaling problems
+    canvas.height = canvas.getContext("2d").canvas.clientHeight;
+    canvas.width = canvas.getContext("2d").canvas.clientWidth;
 });
+
+function getCanvasPos(e){
+    var rect = canvas.getBoundingClientRect();
+    return {x: e.clientX - rect.left, y: e.clientY - rect.top};
+}
+
+function addPoint()
+{
+    points[points.length] = pos;
+    redraw();
+}
+
+function redraw()
+{
+    var ctx=canvas.getContext("2d");
+    clearCanvas();
+    ctx.save();
+    ctx.fillStyle="#000000";
+    for(i = 0; i < points.length; i++){
+        ctx.fillRect(points[i].x - 1,points[i].y - 1,3,3);
+    }
+    ctx.restore();
+}
+
+function clearVoronoiCanvas(){
+    points = new Array();
+    redraw();
+}
+
+function clearCanvas()
+{
+    //var c=$(".panes div:not(:hidden) canvas")[0];
+    var ctx=canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
