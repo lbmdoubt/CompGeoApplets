b6989759db748ac4dfca701aa6d8707b9edcfb46
lbmdoubt <lbmdoubt@gmail.com>
2014-04-08 01:16:22 -0500

    added range selection and x tree drawing

--- scripts/rangeQuery.js
+++ scripts/rangeQuery.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
     var canvas, xCanvas, yCanvas;
     var startTime = -1;
     var selectedPointIndex = -1;
+    var highlightedPointIndex = -1;
 	var maxPoints = 200;
 	var xTree = new Array();
 	var yTree = new Array();
+    var selectRect = null;
+    var isMouseDown = false;
 
     //set up action listeners
     canvas = document.getElementById('rangeQueryCanvas');
@@ -29,6 +32,7 @@ $(document).ready(function(){
     }
 
     function mouseDown(){
+        isMouseDown = true;
         startTime = new Date().getTime();
         selectedPointIndex = -1;
         for(i = 0; i < points.length; i++){
@@ -36,9 +40,13 @@ $(document).ready(function(){
                 selectedPointIndex = i;
             }
         }
+        if(selectedPointIndex < 0){
+            selectRect = {x1:pos.x, y1:pos.y, x2:pos.x, y2:pos.y};
+        }
     }
 
     function mouseUp(){
+        isMouseDown = false;
         addPoint();
         startTime = -1;
         selectedPointIndex = -1;
@@ -49,25 +57,28 @@ $(document).ready(function(){
 		if(points.length > maxPoints){
 			return;
 		}
-        if(selectedPointIndex < 0){
+        endTime = new Date().getTime();
+        if(selectedPointIndex < 0 && endTime - startTime < 200){
             points[points.length] = pos;
         } else {
             endTime = new Date().getTime();
             if(startTime > 0){
-                if(endTime - startTime < 250){
+                if(endTime - startTime < 200){
                     points.splice(selectedPointIndex, 1);
                 }
             }
         }
         //remove duplicate points
         var selectedPoint = selectedPointIndex;
-        if(selectedPoint < 0){
-            //ignore newly added point
-            selectedPoint = points.length - 1
-        }
-        for(i = points.length - 1; i >= 0; i--){
-            if(i != selectedPoint && Math.abs(points[i].x - pos.x) <= 8 && Math.abs(points[i].y - pos.y) <= 8){
-                    points.splice(i, 1);
+        if(endTime - startTime < 200){
+            if(selectedPoint < 0){
+                //ignore newly added point
+                selectedPoint = points.length - 1
+            }
+            for(i = points.length - 1; i >= 0; i--){
+                if(i != selectedPoint && Math.abs(points[i].x - pos.x) <= 8 && Math.abs(points[i].y - pos.y) <= 8){
+                        points.splice(i, 1);
+                }
             }
         }
 		constructXTree();
@@ -78,6 +89,10 @@ $(document).ready(function(){
         endTime = new Date().getTime();
         if(startTime > 0){
             movePoint();
+        }
+        if(selectedPointIndex < 0 && selectRect != null && isMouseDown && endTime - startTime > 200){
+            selectRect.y2 = pos.y;
+            selectRect.x2 = pos.x;
         }
     }
 
@@ -93,11 +108,29 @@ $(document).ready(function(){
         clearCanvas();
         var ctx=canvas.getContext("2d");
         ctx.save();
+        
+        if(selectRect != null){
+            var x = Math.min(selectRect.x1, selectRect.x2);
+            var y = Math.min(selectRect.y1, selectRect.y2);
+            var xDiff = Math.max(selectRect.x1, selectRect.x2) - x;
+            var yDiff = Math.max(selectRect.y1, selectRect.y2) - y;
+            ctx.globalAlpha = 0.2;
+            ctx.fillStyle = "#0000FF";
+            ctx.fillRect(x,y,xDiff,yDiff);
+            ctx.globalAlpha = 1.0;
+            ctx.strokeStyle = "#0000FF";
+            ctx.rect(x,y,xDiff,yDiff);
+            ctx.stroke();
+            ctx.strokeStyle = "#000000";
+        }
+        
+        highlightedPointIndex = -1;
         for(i = 0; i < points.length; i++){
             if(Math.abs(points[i].x - pos.x) <= 4 && Math.abs(points[i].y - pos.y) <= 4){
                 if(selectedPointIndex < 0 || i == selectedPointIndex){
 					ctx.fillStyle="#FF0000";
                     ctx.fillRect(points[i].x - 2,points[i].y - 2,5,5);
+                    highlightedPointIndex = i;
                 }
             } else {
 				ctx.fillStyle="#000000";
@@ -121,6 +154,8 @@ $(document).ready(function(){
 	
     function clearRangeQueryCanvas(){
         points = new Array();
+        xTree = new Array();
+        yTree = new Array();
         redraw();
     }
 
@@ -153,10 +188,30 @@ $(document).ready(function(){
 			xCtx.rect(xTree[0].x - 5, xTree[0].y - 5, 10, 10);
 		}
 		 
-		xCtx.stroke();
 		
-		for(i=1; i < xTree.length; i++){
-			
+		for(i=0; i < xTree.length; i++){
+            xCtx.beginPath();
+            if(xTree[i].isLeaf && xTree[i].refPoint == highlightedPointIndex){
+				xCtx.strokeStyle="#FF0000";
+            } else {
+				xCtx.strokeStyle="#222222";
+            }
+			if(xTree[i].isLeaf){
+                xCtx.arc(xTree[i].x,xTree[i].y,5,0,2*Math.PI);
+            } else {
+                xCtx.rect(xTree[i].x - 5, xTree[i].y - 5, 10, 10);
+                
+                //calculate positions of children
+                var xChange = xTree[i].rightChild - xTree[i].leftChild;
+                //left child
+                xTree[xTree[i].leftChild].y = xTree[i].y + 20;
+                xTree[xTree[i].leftChild].x = xTree[i].x - xChange * 10;
+                
+                //right child
+                xTree[xTree[i].rightChild].y = xTree[i].y + 20;
+                xTree[xTree[i].rightChild].x = xTree[i].x + xChange * 10;
+            }
+            xCtx.stroke();
 		}
 		
 		xCtx.restore();
